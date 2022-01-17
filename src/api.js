fb727669da60a86a57c3c76a17dff040818db8df
joelrummel <joelrummel@yahoo.com>
2022-01-17 17:28:41 -0500

    Add example API call

--- src/api.js
+++ src/api.js
@@ -1,3 +1,19 @@
-const API_URL = "http://ec2-18-118-95-190.us-east-2.compute.amazonaws.com:3000/";
+// const API_URL = "http://ec2-18-118-95-190.us-east-2.compute.amazonaws.com:3000";
+const API_URL = "http://localhost:3000";
+
+const request = async (endpoint, body) => {
+    const res = await fetch(API_URL + endpoint, {
+        method: body ? 'POST' : 'GET',
+        body: body ? JSON.stringify(body) : undefined,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return res.json();
+};
 
 // TODO: export API functions here
+export const testApi = async () => {
+    const res = await request('/users/add', { user: { name: "Poopyhead", id: "abc" } });
+    console.log(res);
+};
