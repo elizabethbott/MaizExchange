2e451fdc66766d8f890cf4f6670fb53ed1a30a8f
joelrummel <joelrummel@yahoo.com>
2022-01-28 13:47:44 -0500

    Remove login button from home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,18 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Text, View } from 'react-native';
-import LogInOrOutButton from '../components/LogInOrOutButton';
-import UserContext from '../contexts/UserContext';
 
 const HomeScreen = () => {
-    const { user } = useContext(UserContext);
-
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
-                {user ? `Logged in as ${user.firstName} ${user.lastName}` :
-                    "Log in with your UMich Google account:"}
+                This is home screen hello
             </Text>
-            <LogInOrOutButton />
         </View>
     );
 };
