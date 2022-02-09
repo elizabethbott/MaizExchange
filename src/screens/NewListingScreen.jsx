fce4c3968898920be80f85a136154f9853cb2c83
joelrummel <joelrummel@yahoo.com>
2022-02-09 16:19:25 -0500

    Fix formatting/unused imports

--- src/screens/NewListingScreen.jsx
+++ src/screens/NewListingScreen.jsx
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Text, View, StyleSheet, TextInput, ScrollView } from 'react-native';
-import UserContext from '../contexts/UserContext';
 import { RadioButton, Button } from 'react-native-paper';
-import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
 
 const NewListingScreen = () => {
-    const { user } = useContext(UserContext);
     const [checked, setChecked] = React.useState('first');
     const [description, setDescription] = React.useState('');
     const [category, setCategory] = React.useState('');
     const [price, setPrice] = React.useState(0);
 
-    const text = "test";
     const press = () => {
         alert('button pressed');
         alert(description);
@@ -30,99 +26,94 @@ const NewListingScreen = () => {
     //define new state variables for each TextInput
     return (
         <ScrollView>
-            <View style={{alignItems: 'center'}}>
-            <View style={styles.container}>
-            <Text style={styles.sectionheaders}>Add Description</Text>
-            <TextInput 
-                style={styles.textfields}
-                value={description}
-                placeholder="Description"
-                onChangeText={setDescription}
-            />
-            
+            <View style={{ alignItems: 'center' }}>
+                <View style={styles.container}>
+                    <Text style={styles.sectionheaders}>Add Description</Text>
+                    <TextInput
+                        style={styles.textfields}
+                        value={description}
+                        placeholder="Description"
+                        onChangeText={setDescription}
+                    />
 
-            <Text style={styles.sectionheaders}>Type</Text>
-            <View style={styles.radiobutton}><Text>Tickets</Text><RadioButton
-                value="first"
-                status={ checked === 'first' ? 'checked' : 'unchecked' }
-                onPress={() => setChecked('first')}
-                color="#00274C"
-            /></View>
+                    <Text style={styles.sectionheaders}>Type</Text>
+                    <View style={styles.radiobutton}><Text>Tickets</Text><RadioButton
+                        value="first"
+                        status={checked === 'first' ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked('first')}
+                        color="#00274C"
+                    /></View>
 
-            {
-                ( checked === 'first') && 
-                <View style={styles.categories}>
-                <Text>Category</Text>
-                <TextInput 
-                    style={styles.textfields}
-                    value={category}
-                    onChangeText={setCategory}
-                    placeholder="Category"
+                    {
+                        (checked === 'first') &&
+                        <View style={styles.categories}>
+                            <Text>Category</Text>
+                            <TextInput
+                                style={styles.textfields}
+                                value={category}
+                                onChangeText={setCategory}
+                                placeholder="Category"
 
-                />
+                            />
+                        </View>
+                    }
+
+                    <View style={styles.radiobutton}><Text>Textbooks</Text><RadioButton
+                        value="second"
+                        status={checked === 'second' ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked('second')}
+                        color="#00274C"
+                    /></View>
+
+                    {
+                        (checked === 'second') &&
+                        <View style={styles.categories}>
+                            <Text>Category</Text>
+                            <TextInput
+                                style={styles.textfields}
+                                value={category}
+                                onChangeText={setCategory}
+                                placeholder="Category"
+                            />
+                        </View>
+                    }
+
+                    <View style={styles.radiobutton}><Text>Other</Text><RadioButton
+                        value="third"
+                        status={checked === 'third' ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked('third')}
+                        color="#00274C"
+                    /></View>
+
+                    {
+                        (checked === 'third') &&
+                        <View style={styles.categories}>
+                            <Text>Category</Text>
+                            <TextInput
+                                style={styles.textfields}
+                                value={category}
+                                onChangeText={setCategory}
+                                placeholder="Category"
+                            />
+                        </View>
+                    }
+
+                    <Text style={styles.sectionheaders}>Price</Text>
+                    <TextInput
+                        style={styles.textfields}
+                        value={price}
+                        keyboardType='numeric'
+                        onChangeText={setPrice}
+                        placeholder="$$"
+                        maxLength={10}
+                    />
+
+                    <View style={styles.buttonboi}><Button onPress={press} icon="card-plus" mode="contained" color="#FFCB05">
+                        Create Listing
+                    </Button></View>
                 </View>
-            }
-
-            
-            <View style={styles.radiobutton}><Text>Textbooks</Text><RadioButton
-                value="second"
-                status={ checked === 'second' ? 'checked' : 'unchecked' }
-                onPress={() => setChecked('second')}
-                color="#00274C"
-            /></View>
-
-            {   
-                ( checked === 'second') && 
-                <View style={styles.categories}>
-                <Text>Category</Text>
-                <TextInput 
-                    style={styles.textfields}
-                    value={category}
-                    onChangeText={setCategory}
-                    placeholder="Category"
-                />
-                </View>
-            }
-
-            <View style={styles.radiobutton}><Text>Other</Text><RadioButton
-                value="third"
-                status={ checked === 'third' ? 'checked' : 'unchecked' }
-                onPress={() => setChecked('third')}
-                color="#00274C"
-            /></View>
-            {
-                ( checked === 'third') && 
-                <View style={styles.categories}>
-                <Text>Category</Text>
-                <TextInput 
-                    style={styles.textfields}
-                    value={category}
-                    onChangeText={setCategory}
-                    placeholder="Category"
-                />
-                </View>
-            }
-            
-
-            <Text style={styles.sectionheaders}>Price</Text>
-            <TextInput 
-                style={styles.textfields}
-                value={price}
-                keyboardType='numeric'
-                onChangeText={setPrice}
-                placeholder="$$"
-                maxLength={10}
-            />
-            
-            <View style={styles.buttonboi}><Button onPress={press} icon="card-plus" mode="contained" color="#FFCB05">
-                Create Listing
-            </Button></View>
-            
-          </View>
-        
-          </View>
+            </View>
         </ScrollView>
-        
     );
 }
 
@@ -134,24 +125,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         width: '50%'
     },
-
     radiobutton: {
-        flexDirection: 'row', 
-        width: '100%', 
+        flexDirection: 'row',
+        width: '100%',
         height: 40,
         borderTopWidth: 1,
         padding: 8,
         alignItems: 'center',
         justifyContent: 'space-between'
     },
-
     sectionheaders: {
         fontSize: 12,
         fontWeight: "900",
         marginTop: 16,
         marginBottom: 4
     },
-
     categories: {
         paddingLeft: 32,
         fontSize: 10,
@@ -159,18 +147,15 @@ const styles = StyleSheet.create({
         marginTop: 16,
         marginBottom: 16,
     },
-
     textfields: {
-        height: 40, 
-        width: '75%', 
-        borderColor: '#00274C', 
-        borderWidth: 1, 
+        height: 40,
+        width: '75%',
+        borderColor: '#00274C',
+        borderWidth: 1,
         //backgroundColor: '#D3D3D3', 
-        padding:4,
+        padding: 4,
         textShadowColor: 'rgba(0, 0, 0, 0.75)',
     },
-
-
     buttonboi: {
         margin: 8,
         marginRight: 50,
