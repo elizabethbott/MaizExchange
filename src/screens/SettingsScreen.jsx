8a6d6ac1457c8667fbe627b1ecb325d83bee2e03
hstauff <hstauff@umich.edu>
2022-02-16 15:46:47 -0500

    MVP1 profile page

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import ExampleComponent from '../components/ExampleComponent';
+import UserContext from '../contexts/UserContext';
+import userlogo from '../images/userlogo.png';
 
 const SettingsScreen = () => {
+    const { user } = useContext(UserContext);
+    
     return (
         <View style={styles.container}>
-            <Text>Example screen for settings - hello world!</Text>
+            <Text>Example screen for profile - hello world!</Text>
             <ExampleComponent />
+            <br></br>
+            <img style={{width: '100px', height: '100px',}} src={userlogo} alt="this is user image" />
+            <Text style={styles.sectionheaders}>{user.firstName} {user.lastName}</Text>
+            <Text style={styles.sectionheaders}>Contact: {user.email}</Text>
         </View>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -18,6 +27,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    sectionheaders: {
+        fontSize: 12,
+        fontWeight: "900",
+        marginTop: 16,
+        marginBottom: 4
+    },
+
 });
 
 export default SettingsScreen;
