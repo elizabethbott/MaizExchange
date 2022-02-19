cbf5d8092e7b09b8cac4e8ef07402584140ad899
joelrummel <joelrummel@yahoo.com>
2022-02-19 17:14:32 -0500

    Add animated dropdown component and use in new listing screen

--- src/screens/NewListingScreen.jsx
+++ src/screens/NewListingScreen.jsx
@@ -1,24 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, TextInput, ScrollView } from 'react-native';
-import UserContext from '../contexts/UserContext';
-import { RadioButton, Button } from 'react-native-paper';
-import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
+import { Button } from 'react-native-paper';
+import EnIcon from 'react-native-vector-icons/Entypo';
+import AppStyle from '../AppStyle';
+import Dropdown from '../components/Dropdown';
 
+const categories = [
+    { label: "Ticket", value: "ticket", icon: "ticket" },
+    { label: "Textbook", value: "textbook", icon: "book" },
+    { label: "Other/Misc.", value: "other", icon: "box" }
+];
 
 const NewListingScreen = () => {
-    const { user } = useContext(UserContext);
-    const [checked, setChecked] = React.useState('');
-    const [description, setDescription] = React.useState('');
-    const [category, setCategory] = React.useState('');
-    const [price, setPrice] = React.useState('');
+    const [checked, setChecked] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
 
-    const text = "test";
     const press = () => {
         alert('button pressed');
         alert(description);
-        alert(checked);
-        alert(category);
-        alert(price);
         setDescription('');
         setChecked("first");
         setCategory('');
@@ -28,73 +29,59 @@ const NewListingScreen = () => {
     const Category = () => {
         return (
             <View style={styles.categories}>
-            <Text>Category</Text>
-            <TextInput 
-                style={styles.categoryfields}
-                value={category}
-                onChangeText={setCategory}
-                placeholder="Category"
-    
-            />
+                <Text>Category</Text>
+                <TextInput
+                    style={styles.categoryfields}
+                    value={category}
+                    onChangeText={setCategory}
+                    placeholder="Category"
+                />
             </View>
         );
-    }
+    };
 
-    //TextInput onchange (calls function on every key stroke) & value props
-    // store value from onChange to state 
-
-    //define new state variables for each TextInput
     return (
-        <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView>
+            <Text style={AppStyle.classes.header}>
+                What would you like to sell?
+            </Text>
             <Text style={styles.sectionheaders}>Add Description</Text>
-            <TextInput 
+            <TextInput
                 style={styles.textfields}
                 value={description}
                 placeholder="Description"
                 onChangeText={setDescription}
             />
-            
 
             <Text style={styles.sectionheaders}>Type</Text>
-            <View style={styles.radiobutton}><Text>Tickets</Text><RadioButton
-                value="first"
-                status={ checked === 'first' ? 'checked' : 'unchecked' }
-                onPress={() => setChecked('first')}
-                color="#00274C"
-                uncheckedColor='#EAE8E4'
-            /></View>
-
             {
-                ( checked === 'first') && Category()
+                categories.map((cat, i) => (
+                    <Dropdown
+                        topBorder={i === 0}
+                        title={(
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <EnIcon name={cat.icon} size={16} color="black" style={{ marginRight: 5 }} />
+                                <Text>{cat.label}</Text>
+                            </View>
+                        )}
+                    >
+                        <Category />
+                    </Dropdown>
+                ))
             }
 
-            
-            <View style={styles.radiobutton}><Text>Textbooks</Text><RadioButton
-                value="second"
-                status={ checked === 'second' ? 'checked' : 'unchecked' }
-                onPress={() => setChecked('second')}
-                color="#00274C"
-                uncheckedColor='#EAE8E4'
-            /></View>
-
-            {   
-                ( checked === 'second') && Category()
-            }
-
-            <View style={styles.radiobutton}><Text>Other</Text><RadioButton
+            {/*<View style={styles.radiobutton}><Text>Other</Text><RadioButton
                 value="third"
-                status={ checked === 'third' ? 'checked' : 'unchecked' }
+                status={checked === 'third' ? 'checked' : 'unchecked'}
                 onPress={() => setChecked('third')}
                 color="#00274C"
                 uncheckedColor='#EAE8E4'
-            /></View>
-            {
-                ( checked === 'third') && Category()
-            }
-            
+        /></View>*/}
 
-            <Text style={styles.sectionheaders}>Price</Text>
-            <TextInput 
+            <View>
+                <Text style={styles.sectionheaders}>Price</Text>
+            </View>
+            <TextInput
                 style={styles.textfields}
                 value={price}
                 keyboardType='numeric'
@@ -102,29 +89,20 @@ const NewListingScreen = () => {
                 placeholder="$$"
                 maxLength={10}
             />
-            
-            <View style={styles.buttonboi}><Button onPress={press} icon="card-plus" mode="contained" color="#FFCB05">
-                Create Listing
-            </Button></View>
-            
+
+            <View style={styles.buttonboi}>
+                <Button onPress={press} icon="card-plus" mode="contained" color="#FFCB05">
+                    Create Listing
+                </Button>
+            </View>
         </ScrollView>
-        
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        //width: '50%',
-        //marginLeft: '25%',
-    },
-
     radiobutton: {
-        flexDirection: 'row', 
-        width: '100%', 
+        flexDirection: 'row',
+        width: '100%',
         height: 70,
         borderTopWidth: 1,
         padding: 8,
@@ -132,7 +110,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         color: '#EAE8E4',
     },
-
     sectionheaders: {
         fontSize: 12,
         fontWeight: "900",
@@ -140,7 +117,6 @@ const styles = StyleSheet.create({
         marginBottom: 4,
         marginLeft: 8,
     },
-
     categories: {
         paddingLeft: 32,
         fontSize: 10,
@@ -149,29 +125,26 @@ const styles = StyleSheet.create({
         marginBottom: 16,
         width: '75%',
     },
-
     textfields: {
-        height: 40, 
-        width: '75%', 
-        borderColor: '#00274C', 
-        borderWidth: 1, 
+        height: 40,
+        width: '75%',
+        borderColor: '#00274C',
+        borderWidth: 1,
         //backgroundColor: '#D3D3D3', 
-        padding:4,
+        padding: 4,
         textShadowColor: 'rgba(0, 0, 0, 0.75)',
         marginLeft: 8,
     },
-
     categoryfields: {
-        height: 40, 
-        width: '100%', 
-        borderColor: '#00274C', 
-        borderWidth: 1, 
+        height: 40,
+        width: '100%',
+        borderColor: '#00274C',
+        borderWidth: 1,
         //backgroundColor: '#D3D3D3', 
-        padding:4,
+        padding: 4,
         textShadowColor: 'rgba(0, 0, 0, 0.75)',
         marginLeft: 8,
     },
-
     buttonboi: {
         margin: 16,
     }
